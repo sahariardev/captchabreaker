52420e899c730e7ea64c210a0e61432984a085c1
sahariardev <sahariaralamk@gmail.com>
2020-05-10 20:27:32 +0600

    Added capchahelper

---
+++ captchahelper.py
@@ -0,0 +1,16 @@
+import imutils
+import cv2
+
+
+def preprocess(image, width, height):
+    (h, w) = image.shape[:2]
+    if w > h:
+        image = imutils.resize(image, width=width)
+    else:
+        image = imutils.resize(image, height=height)
+    padW = int((width - image.shape[1]) / 2)
+    padH = int((height - image.shape[0]) / 2)
+    image = cv2.copyMakeBorder(image,padH,padH,padW,padW,cv2.BORDER_REPLICATE)
+    image = cv2.resize(image,(width, height))
+    return image
+
